6281fbf9248052aa912ee84ee3a9d869e497ec3c
Wiola Janczura <wiola.janczura@wp.pl>
2022-11-22 11:31:14 +0100

    task 1 done

---
+++ js/01-gallery.js
@@ -0,0 +1,33 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const instance = basicLightbox.create(
+  `
+    <div class="modal">
+        <img src="" alt="Large img"/>
+    </div>
+`,
+  {
+    closable: true,
+  }
+);
+
+const gallery = document.querySelector("div.gallery");
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+  const clickedElement = event.target;
+  if (clickedElement.nodeName !== "IMG") return;
+
+  const largeURL = clickedElement.dataset.source;
+
+  const modal = instance.element();
+  const modalImg = modal.querySelector("img");
+  modalImg.src = largeURL;
+
+  const description = clickedElement.alt;
+  modalImg.alt = description;
+
+  instance.show();
+});
+
+console.log(galleryItems);
