63b8378cbdf34f7ebe8a4198d493befaf45eefa4
Wiola Janczura <wiola.janczura@wp.pl>
2022-11-28 11:00:07 +0100

    add task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const lightbox = new SimpleLightbox("gallery a", {captionDelay: 250, captionData:"alt"});
+
+const gallery = document.querySelector("ul.gallery");
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+ 
+  const clickedElement = event.target;
+  if (clickedElement.nodeName !== "IMG") return;
+
+  const largeURL = clickedElement.dataset.source;
+  const description = clickedElement.alt;
+  lightbox.open();
+});
+
+document.addEventListener("keydown", (event)=> {
+    if (event.key === "Escape" && instance.visible()) instance.close();
+})
+
+
+
 
 console.log(galleryItems);
