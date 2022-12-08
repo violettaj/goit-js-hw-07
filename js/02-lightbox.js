e73abbfc2f97725f8f7a6dbaa9c4fda84913cb04
Wiola Janczura <wiola.janczura@wp.pl>
2022-12-09 00:37:55 +0100

    fix task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,9 +1,6 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-const lightbox = new SimpleLightbox(".gallery a", {
-  captionDelay: 250,
-  captionData: "alt",
-});
+
 
 const gallery = document.querySelector(".gallery");
 
@@ -19,19 +16,8 @@ galleryItems.forEach((element) => {
   items.append(images);
 });
 
-gallery.addEventListener("click", (event) => {
-  event.preventDefault();
-
-  const clickedElement = event.target;
-  if (clickedElement.nodeName !== "IMG") return;
-
-  const largeURL = clickedElement.dataset.source;
-  const description = clickedElement.alt;
-  lightbox.open();
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionData: "alt",
 });
 
-document.addEventListener("keydown", (event) => {
-  if (event.key === "Escape" && instance.visible()) instance.close();
-});
-
-console.log(galleryItems);
